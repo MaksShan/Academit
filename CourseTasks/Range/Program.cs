b0d260a3a339f6e7fdc4f6c8927357633bae9376
shandrin_max@mail.ru <shandrin_max@mail.ru>
2021-07-18 15:44:06 +0700

    Создан проект Range

---
+++ CourseTasks/Range/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало диапазона первого интервала: ");
+            double from1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец диапазона первого интервала: ");
+            double to1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите начало диапазона второго интервала: ");
+            double from2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец диапазона второго интервала: ");
+            double to2 = double.Parse(Console.ReadLine());
+
+            Range range1 = new Range(from1, to1);
+            Range range2 = new Range(from2, to2);
+
+            Console.WriteLine($"Длина первого диапазона чисел: {range1.GetRange()}");
+            Console.WriteLine($"Длина второго диапазона чисел: {range2.GetRange()}");
+
+            Console.Write("Введите число для проверки на пренадлежность к диапазонам: ");
+            double number = double.Parse(Console.ReadLine());
+
+            bool isNumberInsideRange1 = range1.IsInside(number);
+            bool isNumberInsideRange2 = range2.IsInside(number);
+
+            if (isNumberInsideRange1)
+            {
+                Console.WriteLine($"Число {number} принадлежит к введённому диапазону первого интервала");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} НЕ принадлежит к введённому диапазону первого интервала");
+            }
+
+            if (isNumberInsideRange2)
+            {
+                Console.WriteLine($"Число {number} принадлежит к введённому диапазону второго интервала");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} НЕ принадлежит к введённому диапазону второго интервала");
+            }
+
+            Range crossingRange = range1.GetCrossingInterval(range2);
+
+            if (crossingRange == null)
+            {
+                Console.WriteLine("Интервалы не пересакаются!");
+            }
+            else
+            {
+                Console.WriteLine($"Интервал пересечения двух интервалов: [{crossingRange.From} : {crossingRange.To}]");
+            }
+
+            Range[] combinedInterval = range1.GetCombinedInterval(range2);
+
+            Console.Write("Объединенный интервал двух интервалов: ");
+
+            if (combinedInterval.Length == 1)
+            {
+                Console.WriteLine($"[{combinedInterval[0].From} : {combinedInterval[0].To}]");
+            }
+            else
+            {
+                Console.WriteLine($"[{combinedInterval[0].From} : {combinedInterval[0].To} , {combinedInterval[1].From} : {combinedInterval[1].To}]");
+            }
+
+            Range[] intervalDifference = range1.GetIntervalDifference(range2);
+
+            Console.Write("Разность интервалов: ");
+
+            if (intervalDifference == null)
+            {
+                Console.WriteLine("0");
+            }
+            else if (intervalDifference.Length == 1)
+            {
+                Console.WriteLine($"[{intervalDifference[0].From} : {intervalDifference[0].To}]");
+            }
+            else
+            {
+                Console.WriteLine($"[{intervalDifference[0].From} : {intervalDifference[0].To} , {intervalDifference[1].From} : {intervalDifference[1].To}]");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        public double From { get; private set; }
+        public double To { get; private set; }
+
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetRange()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return number >= From && number <= To;
+        }
+
+        private bool isCrossing = true;
+        public Range GetCrossingInterval(Range range2)
+        {
+            if (From < range2.To && To > range2.From)
+            {
+                double newFrom = Math.Max(From, range2.From);
+                double newTo = Math.Min(To, range2.To);
+
+                return new Range(newFrom, newTo);
+            }
+
+            isCrossing = false;
+
+            return null;
+        }
+
+        public Range[] GetCombinedInterval(Range range2)
+        {
+            if (From <= range2.To && To >= range2.From)
+            {
+                double newFrom = Math.Min(From, range2.From);
+                double newTo = Math.Max(To, range2.To);
+
+                return new Range[] { new Range(newFrom, newTo) };
+            }
+
+            if (From <= range2.From)
+            {
+                return new Range[] { new Range(From, To), new Range(range2.From, range2.To) };
+            }
+
+            return new Range[] { new Range(range2.From, range2.To), new Range(From, To) };
+        }
+
+        public Range[] GetIntervalDifference(Range range2)
+        {
+            if (range2.From <= From && range2.To >= To) // равны или 2 больше 1
+            {
+                return null;
+            }
+
+            if (!isCrossing) // если не пересекаются
+            {
+                return new Range[] { new Range(From, To) };
+            }
+
+            if (From < range2.From && To > range2.To) // если второй внутри первого
+            {
+                return new Range[] { new Range(From, range2.From - 1), new Range(range2.To + 1, To) };
+            }
+
+            if (From < range2.From && To < range2.To) // если второй пересекает справа
+            {
+                return new Range[] { new Range(From, range2.From - 1) };
+            }
+
+            if (From > range2.From && To > range2.To) // если второй пересекает слева
+            {
+                return new Range[] { new Range(range2.To + 1, To) };
+            }
+
+            return new Range[] { new Range(From, range2.From), new Range(range2.To, To) };
+        }
+    }
+}
