4c68fdf3d53830a02c6b8e658eee407db9d9c89d
shandrin_max@mail.ru <shandrin_max@mail.ru>
2021-08-03 21:56:52 +0700

    Создал проект Vector

---
+++ CourseTasks/Vector/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vector1Array = { 1, -2, 4, 6, 9 };
+            Vector vector1 = new Vector(vector1Array);
+
+            double[] vector2Array = { 3, 0.2, -1, 9 };
+            Vector vector2 = new Vector(vector2Array);
+
+            Vector.Check(vector1, vector2);
+
+            Console.WriteLine($"Размерность вектора {vector1}: {vector1.GetSize()}");
+            Console.WriteLine($"Размерность вектора {vector2}: {vector2.GetSize()}");
+
+            Console.WriteLine("Результат сложения векторов: " + vector1.Multiplication(vector2));
+
+            Console.WriteLine("Результат вычитания из результата второй вектор: " + vector1.Subtraction(vector2));
+
+            Console.WriteLine($"Результат умножения полученного вектора на скаляр {5}: {vector1.ScalarMultiplication(5)}");
+
+            Console.WriteLine($"Разворот вектора: " + vector1.Reversal());
+
+            Console.WriteLine("Длинна вектора: " + vector1.GetLength());
+
+            vector1.GetSet(3);
+            Console.WriteLine($"Вектор после замен компонента по индексу {3}: {vector1}");
+
+            if (vector1.Equals(vector2))
+            {
+                Console.WriteLine("Векторы равны");
+            }
+            else
+            {
+                Console.WriteLine("Векторы не равны");
+            }
+
+            Console.WriteLine($"Сложение векторов {vector1} и {vector2} с помошью статического метода = {Vector.StaticMultiplication(vector1, vector2)}");
+
+            Console.WriteLine($"Вычитание векторов {vector1} и {vector2} с помошью статического метода = {Vector.StaticSubtraction(vector1, vector2)}");
+
+            Console.WriteLine($"Скалярное произведение векторов {vector1} и {vector2} = {Vector.ScalarProduct(vector1, vector2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CourseTasks/Vector/Vector.cs
@@ -0,0 +1,240 @@
+﻿using System;
+
+namespace Vector
+{
+    class Vector
+    {
+        private double[] vector;
+
+        public Vector(int n)
+        {
+            vector = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vector[i] = 0;
+            }
+        }
+
+        public Vector(Vector inputVector)
+        {
+            vector = new double[inputVector.vector.Length];
+            vector = inputVector.vector;
+        }
+
+        public Vector(double[] inputArray)
+        {
+            vector = new double[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                vector[i] = inputArray[i];
+            }
+        }
+
+        public Vector(int n, double[] inputArray)
+        {
+            vector = new double[n];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i < n)
+                {
+                    vector[i] = inputArray[i];
+                }
+                else
+                {
+                    vector[i] = 0;
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return vector.Length;
+        }
+
+        public Vector Multiplication(Vector inputVector)
+        {
+            for (int i = 0; i < inputVector.vector.Length; i++)
+            {
+                vector[i] += inputVector.vector[i];
+            }
+
+            return new Vector(vector);
+        }
+
+        public Vector Subtraction(Vector inputVector)
+        {
+            for (int i = 0; i < inputVector.vector.Length; i++)
+            {
+                vector[i] -= inputVector.vector[i];
+            }
+
+            return new Vector(vector);
+        }
+
+        public Vector ScalarMultiplication(int scalar)
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] *= scalar;
+            }
+
+            return new Vector(vector);
+        }
+
+        public Vector Reversal()
+        {
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] *= -1;
+            }
+
+            return new Vector(vector);
+        }
+
+        public double GetLength()
+        {
+            double squareSum = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                squareSum += vector[i] * vector[i];
+            }
+
+            return Math.Sqrt(squareSum);
+        }
+
+        public Vector GetSet(int index)
+        {
+            Console.WriteLine($"Компонент по индексу {index} = {vector[index]}");
+
+            Console.Write("Введите другой компонент для замены: ");
+            vector[index] = double.Parse(Console.ReadLine());
+
+            return new Vector(vector);
+        }
+
+        public static Vector StaticMultiplication(Vector vector1, Vector vector2)
+        {
+            Vector newVector = new Vector(vector1.vector.Length);
+
+            for (int i = 0; i < vector1.vector.Length; i++)
+            {
+                newVector.vector[i] = vector1.vector[i] + vector2.vector[i];
+            }
+
+            return newVector;
+        }
+
+        public static Vector StaticSubtraction(Vector vector1, Vector vector2)
+        {
+            Vector newVector = new Vector(vector1.vector.Length);
+
+            for (int i = 0; i < vector1.vector.Length; i++)
+            {
+                newVector.vector[i] = vector1.vector[i] - vector2.vector[i];
+            }
+
+            return newVector;
+        }
+
+        public static double ScalarProduct(Vector vector1, Vector vector2)
+        {
+            double scalarProduct = 0;
+
+            for (int i = 0; i < vector1.vector.Length; i++)
+            {
+                scalarProduct += vector1.vector[i] * vector2.vector[i];
+            }
+
+            return scalarProduct;
+        }
+
+        public void Modification(int length)
+        {
+            Vector newVector = new Vector(length);
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                newVector.vector[i] = vector[i];
+            }
+
+            for (int i = vector.Length; i < length; i++)
+            {
+                newVector.vector[i] = 0;
+            }
+
+            vector = newVector.vector;
+        }
+
+        public static void Check(Vector vector1, Vector vector2)
+        {
+            if (vector1.vector.Length == 0 || vector2.vector.Length == 0)
+            {
+                throw new ArgumentException("Размерность одного из векторов = 0");
+            }
+
+            if (vector1.vector.Length != vector2.vector.Length)
+            {
+                if (vector1.vector.Length > vector2.vector.Length)
+                {
+                    vector2.Modification(vector1.vector.Length);
+                }
+                else
+                {
+                    vector1.Modification(vector2.vector.Length);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return '{' + string.Join(", ", vector) + '}';
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Vector inputVector = (Vector)obj;
+
+            if (vector.Length != inputVector.vector.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] != inputVector.vector[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 37;
+            int hash = 1;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                hash += prime * hash + vector[i].GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+}
