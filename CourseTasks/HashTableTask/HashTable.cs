98523e3becdccd4a8ab359f798a620df4bc59118
shandrin_max@mail.ru <shandrin_max@mail.ru>
2021-09-28 19:13:24 +0700

    Создал проект HashTableTask

---
+++ CourseTasks/HashTableTask/HashTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTableTask
+{
+    class HashTable
+    {
+        private List<string>[] table;
+
+        public HashTable(int size)
+        {
+            table = new List<string>[size];
+        }
+
+        public int GetListIndex(string item)
+        {
+            return Math.Abs(item.GetHashCode() % table.Length);
+        }
+
+        public void SetItem(string item)
+        {
+            int index = GetListIndex(item);
+
+            if (table[index] == null)
+            {
+                table[index] = new List<string>();
+
+                table[index].Add(item);
+            }
+            else
+            {
+                table[index].Add(item);
+            }
+        }
+
+        public int GetItemIndex(string searchItem)
+        {
+            int index = GetListIndex(searchItem);
+
+            if (table[index] == null)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < table[index].Count; i++)
+            {
+                if (table[index][i].Equals(searchItem))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public string RemoveItem(string itemToRemove)
+        {
+            int index = GetListIndex(itemToRemove);
+
+            try
+            {
+                table[index].RemoveAt(GetItemIndex(itemToRemove));
+
+                return $"Элемент \"{itemToRemove}\" был удален";
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return $"Элемента \"{itemToRemove}\" не существует";
+            }
+        }
+    }
+}
---
+++ CourseTasks/HashTableTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace HashTableTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable table = new HashTable(100);
+
+            using (StreamReader reader = new StreamReader("..\\..\\..\\Countries.txt"))
+            {
+                string currentLine;
+
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    table.SetItem(currentLine);
+                }
+            }
+
+            string searchItem = "Benin";
+
+            if (table.GetItemIndex(searchItem) == -1)
+            {
+                Console.WriteLine($"Элемента \"{searchItem}\" не существует");
+            }
+            else
+            {
+                Console.WriteLine($"Хэш-код элемента \"{searchItem}\" = {table.GetListIndex(searchItem)}, его индекс в листе: {table.GetItemIndex(searchItem)}");
+
+                Console.WriteLine(table.RemoveItem(searchItem));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
