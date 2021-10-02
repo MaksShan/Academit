b7c5013310a120396d686bad275edb6669a07680
shandrin_max@mail.ru <shandrin_max@mail.ru>
2021-10-02 17:26:04 +0700

    Создал проект ListTask

---
+++ CourseTasks/ListTask/MyList.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListTask
+{
+    public class MyList<T> : IList<T>, IEnumerable<T>
+    {
+        private T[] items = new T[10];
+        private int length;
+
+        public int Count
+        {
+            get { return length; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > length - 1 || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Индекс должен быть больше 0 и меньше {length - 1}, {nameof(index)}");
+                }
+
+                return items[index];
+            }
+            set
+            {
+                if (index > length - 1 || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException($"Индекс должен быть больше 0 и меньше {length - 1}, {nameof(index)}");
+                }
+
+                items[index] = value;
+            }
+        }
+
+        public void Add(T obj)
+        {
+            if (length >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[length] = obj;
+
+            ++length;
+        }
+
+        private void IncreaseCapacity()
+        {
+            T[] old = items;
+
+            items = new T[old.Length * 2];
+
+            Array.Copy(old, 0, items, 0, old.Length);
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > length || index < 0)
+            {
+                throw new IndexOutOfRangeException($"Индекс ({index}) должен быть больше 0 и меньше {length - 1}, {nameof(index)}");
+            }
+            if (index < length - 1)
+            {
+                Array.Copy(items, index + 1, items, index, length - index - 1);
+            }
+
+            --length;
+        }
+
+        public void Insert(int index, T obj)
+        {
+            if (index > length || index < 0)
+            {
+                throw new IndexOutOfRangeException($"Индекс ({index}) должен быть больше 0 и меньше {length - 1}, {nameof(index)}");
+            }
+
+            if (index < length - 1)
+            {
+                if (length >= items.Length)
+                {
+                    IncreaseCapacity();
+                }
+
+                Array.Copy(items, index, items, index + 1, length - index);
+
+                items[index] = obj;
+            }
+
+            ++length;
+        }
+
+        public void Clear()
+        {
+            length = 0;
+        }
+
+        public bool Contains(T obj)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (items[i].Equals(obj))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                array.SetValue(items[i], index++);
+            }
+        }
+
+        public int IndexOf(T obj)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (items[i].Equals(obj))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Remove(T obj)
+        {
+            try
+            {
+                RemoveAt(IndexOf(obj));
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                yield return items[i];
+            }
+        }
+    }
+}
---
+++ CourseTasks/ListTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ListTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine($"Под индексом {3} находитится число: {list[3]}");
+
+            list[3] = 0;
+            Console.WriteLine($"Заменив число под индексом {3} на {0}, получим: {list[3]}");
+
+            Console.WriteLine($"Приведенный список состоит из: {list.Count} чисел");
+
+            list.Add(5);
+            Console.WriteLine($"Добавив к списку число {5}, получим список: {string.Join(", ", list)}");
+
+            list.RemoveAt(2);
+            Console.WriteLine($"Убрав из списка число под индексом {2}, получаем список: {string.Join(", ", list)}");
+
+            list.Remove(5);
+            Console.WriteLine($"Удалив из списка первое вхождение числа {5}, получаем список: {string.Join(", ", list)}");
+
+            list.Insert(0, 0);
+            Console.WriteLine($"После вставки в список числа {0} по  индексу {0}, получим список: {string.Join(", ", list)}");
+
+            if (list.Contains(8))
+            {
+                Console.WriteLine($"Список содержит число {8}");
+            }
+            else
+            {
+                Console.WriteLine($"Список не содержит число {8}");
+            }
+
+            int[] array = new int[10];
+
+            list.CopyTo(array, 2);
+            Console.WriteLine($"Массив с скопированными числами из листа, начиная с индекса {2}: {string.Join(", ", array)}");
+
+            Console.WriteLine($"Индекс числа {2}: {list.IndexOf(2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
