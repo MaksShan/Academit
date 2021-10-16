6dec2db4e69934a8dc3c8d9284b836cf743b84fc
shandrin_max@mail.ru <shandrin_max@mail.ru>
2021-10-16 18:23:38 +0700

    Создал проект Temperature

---
+++ CourseTasks/Temperature/TemperatureConverter.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Temperature
+{
+    partial class TemperatureConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.lableConvertResult = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(123, 99);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(478, 21);
+            this.comboBoxFrom.TabIndex = 0;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(123, 172);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(478, 21);
+            this.comboBoxTo.TabIndex = 1;
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Location = new System.Drawing.Point(3, 103);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(114, 13);
+            this.labelFrom.TabIndex = 2;
+            this.labelFrom.Text = "FROM:";
+            this.labelFrom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTo
+            // 
+            this.labelTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTo.AutoSize = true;
+            this.labelTo.Location = new System.Drawing.Point(3, 176);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(114, 13);
+            this.labelTo.TabIndex = 3;
+            this.labelTo.Text = "TO:";
+            this.labelTo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxValue.Location = new System.Drawing.Point(123, 26);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(478, 20);
+            this.textBoxValue.TabIndex = 4;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ConvertButton.Location = new System.Drawing.Point(0, 294);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(604, 67);
+            this.ConvertButton.TabIndex = 5;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(3, 242);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(114, 29);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Result:";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lableConvertResult
+            // 
+            this.lableConvertResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lableConvertResult.AutoSize = true;
+            this.lableConvertResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lableConvertResult.Location = new System.Drawing.Point(123, 244);
+            this.lableConvertResult.Name = "lableConvertResult";
+            this.lableConvertResult.Size = new System.Drawing.Size(478, 25);
+            this.lableConvertResult.TabIndex = 7;
+            // 
+            // labelValue
+            // 
+            this.labelValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelValue.AutoSize = true;
+            this.labelValue.Location = new System.Drawing.Point(3, 30);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(114, 13);
+            this.labelValue.TabIndex = 8;
+            this.labelValue.Text = "Value:";
+            this.labelValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.labelValue, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lableConvertResult, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelFrom, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelResult, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelTo, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxFrom, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxValue, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxTo, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(604, 294);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // TemperatureConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.ConvertButton);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "TemperatureConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Temperature Converter";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label lableConvertResult;
+        private System.Windows.Forms.Label labelValue;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ CourseTasks/Temperature/TemperatureConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Temperature
+{
+    public partial class TemperatureConverter : Form
+    {
+        public TemperatureConverter()
+        {
+            InitializeComponent();
+
+            string[] listFrom = { "Цельсий", "Фаренгейт", "Кельвин" };
+            string[] listTo = { "Цельсий", "Фаренгейт", "Кельвин" };
+
+            comboBoxFrom.DataSource = listFrom;
+            comboBoxTo.DataSource = listTo;
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            double sourceTemperature = 0;
+            double resultTemperatrure = 0;
+
+            try
+            {
+                sourceTemperature = double.Parse(textBoxValue.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат введенных данных", "Неверный формат", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            if (comboBoxFrom.Text == "Цельсий" && comboBoxTo.Text == "Цельсий")
+            {
+                resultTemperatrure = sourceTemperature;
+            }
+            else if (comboBoxFrom.Text == "Цельсий" && comboBoxTo.Text == "Фаренгейт")
+            {
+                resultTemperatrure = sourceTemperature * 1.8 + 32;
+            }
+            else if (comboBoxFrom.Text == "Цельсий" && comboBoxTo.Text == "Кельвин")
+            {
+                resultTemperatrure = sourceTemperature + 273.15;
+            }
+            else if (comboBoxFrom.Text == "Фаренгейт" && comboBoxTo.Text == "Фаренгейт")
+            {
+                resultTemperatrure = sourceTemperature;
+            }
+            else if (comboBoxFrom.Text == "Фаренгейт" && comboBoxTo.Text == "Цельсий")
+            {
+                resultTemperatrure = (sourceTemperature - 32) / 1.8;
+            }
+            else if (comboBoxFrom.Text == "Фаренгейт" && comboBoxTo.Text == "Кельвин")
+            {
+                resultTemperatrure = sourceTemperature / 1.8 - 32 + 273;
+            }
+            else if (comboBoxFrom.Text == "Кельвин" && comboBoxTo.Text == "Кельвин")
+            {
+                resultTemperatrure = sourceTemperature;
+            }
+            else if (comboBoxFrom.Text == "Кельвин" && comboBoxTo.Text == "Цельсий")
+            {
+                resultTemperatrure = sourceTemperature - 273.15;
+            }
+            else if (comboBoxFrom.Text == "Кельвин" && comboBoxTo.Text == "Фаренгейт")
+            {
+                resultTemperatrure = 32 + (sourceTemperature - 273) * 1.8;
+            }
+            else
+            {
+                MessageBox.Show("Шкалы температур заданы неверно", "Неверные шкалы", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            lableConvertResult.Text = resultTemperatrure.ToString("0.00");
+        }
+    }
+}
